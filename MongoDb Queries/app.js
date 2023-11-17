3b508e6dcdedab3f55f8e796258ccd8df9789c1e
Asavei16 <asaveiflo@gmail.com>
2023-11-17 12:04:57 +0200

    reading documents

--- MongoDb Queries/app.js
+++ MongoDb Queries/app.js
@@ -88,6 +88,162 @@
 ///////////////////////////////////////////////
 //CRUD => Reading Documents
 ///////////////////////////////////////////////
+// test> use amazon
+// switched to db amazon
+// amazon> db.products.find()
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find().pretty()
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({name:"macbook"})
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   }
+// ]
+// amazon> db.products.find({category:"Electronics"})
+// [
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({}, {name:1})
+// [
+//   { _id: ObjectId("6557351ca2e17dba19106d96"), name: 'macbook' },
+//   { _id: ObjectId("655735a7a2e17dba19106d97"), name: 'iPhone 11' },
+//   { _id: ObjectId("655735a7a2e17dba19106d98"), name: 'Headphone' }
+// ]
+// amazon> db.products.find({}, {name:1, _id:0})
+// [ { name: 'macbook' }, { name: 'iPhone 11' }, { name: 'Headphone' } ]
+// amazon> db.products.find({}, {name:1, _id:0}).limit(2)
+// [ { name: 'macbook' }, { name: 'iPhone 11' } ]
+// amazon> db.products.find({price: {$lt: 150}})
+// [
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({price: {$lte: 150}})
+// [
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({price: {$gt: 800}})
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({price: {$gte: 900}})
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({$and: [{price: {$lt:200}},{category:"Electronics"}]})
+// [
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
+// amazon> db.products.find({$or: [{price: {$gt:500}},{category:"Electronics"}]})
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
 
 ///////////////////////////////////////////////
 //CRUD => Updating Documents
