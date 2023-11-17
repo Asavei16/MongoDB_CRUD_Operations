2c280fabb2af1f835d85536a2f0f262881ea9f98
Asavei16 <asaveiflo@gmail.com>
2023-11-17 11:45:42 +0200

    Creating Documents

--- MongoDb Queries/app.js
+++ MongoDb Queries/app.js
@@ -39,6 +39,51 @@
 ///////////////////////////////////////////////
 //CRUD => Creating Documents
 ///////////////////////////////////////////////
+// test> use amazon
+// switched to db amazon
+// amazon> db.products.insertOne({name: "macbook", price:1500, category: "Computers"})
+// {
+//   acknowledged: true,
+//   insertedId: ObjectId("6557351ca2e17dba19106d96")
+// }
+// amazon> db.products.find()
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   }
+// ]
+// amazon> db.products.insertMany([{name: "iPhone 11", price: 900, category: "Electronics"},{name: "Headphone", price: 100, category:"Electronics"}])
+// {
+//   acknowledged: true,
+//   insertedIds: {
+//     '0': ObjectId("655735a7a2e17dba19106d97"),
+//     '1': ObjectId("655735a7a2e17dba19106d98")
+//   }
+// }
+// amazon> db.products.find()
+// [
+//   {
+//     _id: ObjectId("6557351ca2e17dba19106d96"),
+//     name: 'macbook',
+//     price: 1500,
+//     category: 'Computers'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d97"),
+//     name: 'iPhone 11',
+//     price: 900,
+//     category: 'Electronics'
+//   },
+//   {
+//     _id: ObjectId("655735a7a2e17dba19106d98"),
+//     name: 'Headphone',
+//     price: 100,
+//     category: 'Electronics'
+//   }
+// ]
 
 ///////////////////////////////////////////////
 //CRUD => Reading Documents
